7edc47a1c3e4dc880a7191915bdbf1565c6b7441
alice <alice@baffle.me>
2016-10-17 14:37:14 -0400

    This coder turned coffee into code. You won't believe how she did it!

--- hellofriend.c
+++ hellofriend.c
@@ -3,10 +3,26 @@
 #include <unistd.h>
 
 int parse_request(char *req, int n) {
+    char file[500]; 
+    char file_content[500]; 
+    int file_len; 
+    char *ptr = req; 
+    FILE *fp;
+    
+    memset(file, 0, sizeof(file)); 
+
+    ptr = (char *)ptr + 2;
+    file_len = n - 2 - 5 - 2; 
+    memcpy(file, ptr, file_len); 
+
+    fp = fopen(file, "r"); 
+    if (fp) {
+        memset(file_content, 0, sizeof(file_content)); 
+        fgets(file_content, sizeof(file_content), fp); 
+        printf("%s", file_content); 
+    }
     return 0; 
 }
-
-
 
 int main(int argc, char *argv[]) {
     char buf[2000];
